37432f2af5af6ef59c46ef43bb4c7086485b7e54
rahul <rahulpatil2387@gmail.com>
2014-01-08 16:31:14 +0530

    second commit

--- q1.rb
+++ q1.rb
@@ -1,7 +1,7 @@
 #Question 1:   										
 #Write a program in Ruby to create an array to store 8 names given below. Now delete the 2nd and the 7th names (from the original array) and print the names remaining in the array.       
 #(Hint: Use the arrays concept and the ‘each (iterator)’)
-
+#abhi
 array = ["name1", "name2", "name3", "name4", "name5", "name6", "name7", "name8"]
 array.each_index do |i|
   if array[i] == "name2" ||  array[i]  == "name7"
